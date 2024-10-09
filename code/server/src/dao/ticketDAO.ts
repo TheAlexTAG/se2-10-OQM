4d424ce6639eb5e5abac02530c9457c66b98ea87
Marco Russo s329270 <s329270@studenti.polito.it>
2024-10-09 18:06:27 +0200

    ticketAPI: added additional functions

--- code/server/src/dao/ticketDAO.ts
+++ code/server/src/dao/ticketDAO.ts
@@ -8,10 +8,8 @@ class ticketDAO {
             const sql = `INSERT INTO ticket(ticketID, serviceID, waitlistCode, counterID, servedTime, ticketDate) 
             VALUES(null, ?, ?, null, null, DATETIME('now'))`
             db.run(sql, [serviceID, waitlistCode], (err: Error) => {
-               if(err)
-                    reject(err);
-               else
-                    resolve(); 
+               if(err) reject(err);
+               else resolve(); 
             })
         })
     }
@@ -19,7 +17,7 @@ class ticketDAO {
     getTicketByID(ticketID: number): Promise<Ticket> {
         return new Promise((resolve, reject) => {
             const sql = `SELECT * FROM ticket WHERE ticketID = ?`
-            db.get(sql, [ticketID], (err, row: {
+            db.get(sql, [ticketID], (err: Error, row: {
                 ticketID: number,
                 serviceID: number,
                 waitlistCode: number,
@@ -34,4 +32,36 @@ class ticketDAO {
             })
         })
     }
+
+    getTicketByWaitlistCode(waitlistCode: number): Promise<Ticket> {
+        return new Promise((resolve, reject) => {
+            const sql = `SELECT * FROM ticket WHERE waitlistCode = ? AND strftime('%Y-%m-%d', ticketDate) = DATE('now')` 
+            db.get(sql, [waitlistCode], (err: Error, row: {
+                ticketID: number,
+                serviceID: number,
+                waitlistCode: number,
+                counterID: number | null,
+                servedTime: string | null,
+                ticketDate: string,
+                served: ServedStatus}) => {
+                    if(err) reject(err);
+                    row ? 
+                    resolve(new Ticket(row.ticketID, row.serviceID, row.waitlistCode, row.counterID, row.servedTime, row.ticketDate, row.served)) 
+                    : reject(new TicketNotFoundError());
+            })
+        })
+    }
+
+    setTicketCounter(ticketID: number, counterID: number): Promise<void>{
+        return new Promise((resolve, reject) => {
+            const sql = `UPDATE ticket 
+            SET counterID = ? WHERE ticketID = ?`
+            db.run(sql, [counterID, ticketID], (err: Error) => {
+                if(err) reject(err);
+                else resolve();
+            })
+        })
+    }
 }
+
+export {ticketDAO};
