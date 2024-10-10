d38532362580bcbc7990fb23a4c1ce4385d5cf8f
Marco Russo s329270 <s329270@studenti.polito.it>
2024-10-10 17:58:13 +0200

    ticketDAO: added some functions and specification for the documentation

--- code/server/src/dao/ticketDAO.ts
+++ code/server/src/dao/ticketDAO.ts
@@ -1,8 +1,18 @@
 import { Ticket, ServedStatus } from "../components/ticket";
 import db from "../db/db"
 import { TicketNotFoundError } from "../errors/ticketErrors";
+/**
+ * Represents a DAO for interaction mainly with ticket table in the database.
+ */
 
 class ticketDAO {
+    /**
+     * Adds a ticket in the ticket table.
+     * @param serviceID represents the id of the service asked.
+     * @param waitlistCode represents the code of priority of the ticket in the date of today.
+     * @returns A void promise to check db errors.
+     */
+
     addTicket(serviceID: number, waitlistCode: number): Promise<void> {
         return new Promise((resolve, reject) => {
             const sql = `INSERT INTO ticket(ticketID, serviceID, waitlistCode, counterID, servedTime, ticketDate) 
@@ -13,7 +23,38 @@ class ticketDAO {
             })
         })
     }
-
+    /**
+     * Computes the next code for the queue insertion.
+     * @returns A promise that resolves to lastWaitlistCode + 1 when the queue is not empty, 1 otherwise.
+     */
+    getNextWaitlistCode(): Promise<number> {
+        return new Promise((resolve, reject) => {
+            const sql = `SELECT MAX(waitlistCode) FROM queue`
+            db.get(sql, [], (err: Error, row: number) => {
+                if(err) reject(err)
+                row ? resolve(row + 1) : resolve(1)
+            })
+        })
+    }
+    /**
+     * Adds a ticket to the today queue.
+     * @param serviceID represents the id of the service asked.
+     * @param waitlistCode represents the code of priority of the ticket in the date of today.
+     */
+    addTicketToQueue(serviceID: number, waitlistCode: number): Promise<void> {
+        return new Promise((resolve, reject) => {
+            const sql = `INSERT INTO queue(waitlistCode, serviceID) VALUES(?, ?)`
+            db.run(sql, [waitlistCode, serviceID], (err: Error) => {
+                if(err) reject(err);
+                else(resolve);
+            })
+        })
+    }
+    /**
+     * Gets a ticket given its unique id.
+     * @param ticketID represents the id of the ticket.
+     * @returns A promise that resolves to a Ticket object if it's found, throws the TicketNotFoundError otherwise.
+     */
     getTicketByID(ticketID: number): Promise<Ticket> {
         return new Promise((resolve, reject) => {
             const sql = `SELECT * FROM ticket WHERE ticketID = ?`
@@ -32,7 +73,11 @@ class ticketDAO {
             })
         })
     }
-
+    /**
+     * Gets a ticket given its waitlist code.
+     * @param waitlistCode represents the priority code of the ticket today.
+     * @returns A promise that resolves to a Ticket object if it's found, throws the TicketNotFoundError otherwise.
+     */
     getTicketByWaitlistCode(waitlistCode: number): Promise<Ticket> {
         return new Promise((resolve, reject) => {
             const sql = `SELECT * FROM ticket WHERE waitlistCode = ? AND strftime('%Y-%m-%d', ticketDate) = DATE('now')` 
@@ -51,7 +96,12 @@ class ticketDAO {
             })
         })
     }
-
+    /**
+     * Sets the ticket counter when the customer is called.
+     * @param ticketID represents the id of the ticket.
+     * @param counterID represents the unique id of the counter.
+     * @returns A void promise to check db errors.
+     */
     setTicketCounter(ticketID: number, counterID: number): Promise<void>{
         return new Promise((resolve, reject) => {
             const sql = `UPDATE ticket 
