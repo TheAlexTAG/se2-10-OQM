63aa4eab237867e5659a48d91d1f9d27531f5ec8
“ShayanKh76” <shayan.khalighi@studenti.polito.it>
2024-10-10 21:23:30 +0200

    chore: add router to front

--- code/client/src/main.tsx
+++ code/client/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
@@ -10,7 +11,11 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </Router>
     </Provider>
   </StrictMode>
 );
