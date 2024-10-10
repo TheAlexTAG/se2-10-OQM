b1fdd192e8b55e361085c2671297524f77d14e3b
Marco Russo s329270 <s329270@studenti.polito.it>
2024-10-10 17:13:46 +0200

    backend core: added queue component and added param description in ticket component

--- code/server/src/components/ticket.ts
+++ code/server/src/components/ticket.ts
@@ -1,3 +1,6 @@
+/**
+ * Represents a ticket in the system
+ */
 class Ticket {
     ticketID: number
     serviceID: number
@@ -6,6 +9,17 @@ class Ticket {
     servedTime: string | null
     ticketDate: string
     served: ServedStatus
+
+    /**
+     * Creates a new instance of the Ticket class.
+     * @param ticketID ticket ID, unique in the database
+     * @param serviceID ID of the service asked 
+     * @param waitlistCode is the unique code of the ticket in a specific date 
+     * @param counterID the id of the counter that takes care of the service
+     * @param servedTime the time and date of the end of the service
+     * @param ticketDate time and date when the ticket is taken
+     * @param served indicates the status of the ticket
+     */
 
     constructor(ticketID: number, serviceID: number, waitlistCode: number, counterID: number | null, servedTime: string | null, ticketDate: string, served: ServedStatus) {
         this.ticketID = ticketID;
