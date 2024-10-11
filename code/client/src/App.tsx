9170d7dd2d92b7cebd4fd7f5a8ab5c0fb27ddff5
Alex Barascu <thealextag@gmail.com>
2024-10-11 10:43:05 +0200

    test

--- code/client/src/App.tsx
+++ code/client/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GetTicket from "./components/GetTicket.tsx";
 import CallCustomer from "./components/CallCustomer.tsx";
