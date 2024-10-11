dc950d1044229ec4b0fc2a7a8e38dfa94f743889
ALESSIO VANTAGGI s331392 <s331392@studenti.polito.it>
2024-10-11 20:51:17 +0200

    Backend login

--- code/server/index.ts
+++ code/server/index.ts
@@ -1,12 +1,74 @@
 import express from 'express';
+import { UserDAO } from "./src/dao/userDAO";
+import {UserRoutes} from "./src/routers/userRoutes";
 
-const app = express();
-const PORT = 3001;
+const morgan= require('morgan'); // logging middleware
+const cors= require('cors');
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+const passport= require("passport");
+const LocalStrategy= require("passport-local");
+
+const session= require("express-session"); // Create the session 
+
+// init express
+const app: express.Application = express();
+const port: number= 3001;
+
+// enable cors
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+// set-up the middlewares
+app.use(morgan('dev'));
+app.use(express.json()); // To automatically decode incoming json
+
+/*** Passport ***/
+const DAO= new UserDAO();
+
+// set up the "username and password" login strategy with a function to verify username and password
+passport.use(new LocalStrategy(async function verify(username: string, password: string, callback: any) {
+  const user = await DAO.getUser(username, password);
+  if (!user) {
+    return callback(null, false, { message: "Incorrect username or password" });
+  }
+  return callback(null, user);
+}));
+
+passport.serializeUser((user: any, callback: any) => {
+  callback(null, user.id);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+passport.deserializeUser(async function (id: number, callback: any) {
+  try {
+    const user = await DAO.getUserById(id);
+    callback(null, user);
+  } catch (err) {
+    callback(err, null);
+  }
+});
+
+const isLoggedIn = (req: any, res: any, next: any) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  return res.status(401).json({ error: "Not authorized" });
+}
+
+app.use(session({
+  secret: 'team10-project',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { httpOnly: true }
+}));
+
+// init passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+/* ROUTES */
+new UserRoutes(app, passport, isLoggedIn);
+
+/*** Other express-related instructions ***/
+// activate the server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
