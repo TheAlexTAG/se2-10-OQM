7074cdfa416e1571356f434631a17c09af1f1a02
Simone Scalora <s329444@studenti.polito.it>
2024-10-14 14:28:23 +0200

    Add static frontend files

--- code/client/src/main.tsx
+++ code/client/src/main.tsx
@@ -1,23 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store.ts";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import Login from "./components/Login/Login.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router>
+      <App />
     </Provider>
   </StrictMode>
 );
