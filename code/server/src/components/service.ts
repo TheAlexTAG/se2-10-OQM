01e9c111de078c4f6571e2b8a5d02fd7165f46bf
Marco Russo s329270 <s329270@studenti.polito.it>
2024-10-14 15:38:30 +0200

    service: added full getAllServices routes

--- code/server/src/components/service.ts
+++ code/server/src/components/service.ts
@@ -3,6 +3,7 @@
  */
 class ServiceType {
     id: number
+    tag: string
     name: string
     description: string | null
     serviceTime: number
@@ -10,12 +11,14 @@ class ServiceType {
     /**
      * Creates a new instance of the Service class.
      * @param id Service ID
-     * @param name Name of the service offered 
+     * @param tag service tag
+     * @param name Name of the service offered
      * @param description Description of the service offered 
      * @param serviceTime Service time for request type
      */
-    constructor(id: number, name: string, description: string | null, serviceTime: number) {
+    constructor(id: number, tag: string, name: string, description: string | null, serviceTime: number) {
         this.id = id;
+        this.tag = tag
         this.name = name;
         this.description = description;
         this.serviceTime = serviceTime;
